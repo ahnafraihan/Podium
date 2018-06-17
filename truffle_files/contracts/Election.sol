ba0b800616a238f12d2d8cacc117e4968115304b
Ahnaf Raihan <ahnafraihan@surfer-172-29-10-121-hotspot.internet-for-guests.com>
2018-06-17 20:07:23 +0200

    Adding Election contract

---
+++ truffle_files/contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.23;
+
+contract Election {
+    //TODO: Candidate struct
+    //TODO: Voter struct
+    //TODO: addCandidate (Owned only) function
+    //TODO: viewCandidates function
+    //TODO: vote function (electionActive only)
+    //TODO: startElection function takes duration (Owned only)
+    //TODO: checkRemainingTime
+    //TODO: checkResults (electionComplete only)
+    //TODO: createCandidate (Owned only)
+    //TODO: register (unique, no address should be able to register twice)
+}
